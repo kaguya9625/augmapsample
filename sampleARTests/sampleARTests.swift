e5be840199056d7fe8364f399fa0c8dc53f1f8b7
kazuki <k016c1308@it-neec.jp>
2019-08-22 18:50:38 +0900

    Initial Commit

---
+++ sampleARTests/sampleARTests.swift
@@ -0,0 +1,34 @@
+//
+//  sampleARTests.swift
+//  sampleARTests
+//
+//  Created by kaguya on 2019/08/22.
+//  Copyright © 2019 kaguya. All rights reserved.
+//
+
+import XCTest
+@testable import sampleAR
+
+class sampleARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
