576c4f7e5d3ca32a703c9d457296e83664713355
kazuki <k016c1308@it-neec.jp>
2019-09-03 11:09:26 +0900

    a

--- sampleAR/ViewController.swift
+++ sampleAR/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ARview: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        ARview.session = ARSession()
+        ARview.showsStatistics = true
+        
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        ARview.debugOptions = [.showLightExtents,.showPhysicsFields,.showWorldOrigin,.showCameras,.showBoundingBoxes,.renderAsWireframe]
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        ARview.session.run(configuration)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        ARview.session.pause()
+    }
+    @IBAction func tap(_ sender: Any) {
+        let scene = SCNScene(named:"art.scnassets/star.scn")!
+        let node = scene.rootNode.childNode(withName:"star",recursively:true)
+        node?.scale = SCNVector3(1,1,1)
+        node?.position = SCNVector3(x:0.0,y:0.0,z:-10)
+       
+        ARview.scene.rootNode.addChildNode(node!)
+    }
+        
+    
 }
 
